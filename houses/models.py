028792e949ad58e6ca53cb1144b8fb631ec2155b
Nikita Shultais <nikita@shultais.ru>
2021-02-20 15:27:47 +0200

    Добавил модель House в приложении houses. Создал миграцию.

--- houses/models.py
+++ houses/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class House(models.Model):
+    name = models.CharField("название", max_length=50)
+    price = models.IntegerField("цена")
+    description = models.TextField("описание")
