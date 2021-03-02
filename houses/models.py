8721ca998a1c3ad9819ed5b1549788855bc9edfb
Nikita Shultais <nikita@shultais.ru>
2021-03-02 16:46:38 +0200

    Установил русские имена для приложения и модели Houses

--- houses/models.py
+++ houses/models.py
@@ -5,3 +5,7 @@ class House(models.Model):
     name = models.CharField("название", max_length=50)
     price = models.IntegerField("цена")
     description = models.TextField("описание")
+
+    class Meta:
+        verbose_name = "дом"
+        verbose_name_plural = "дома"
