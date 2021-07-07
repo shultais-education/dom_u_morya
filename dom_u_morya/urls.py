58bae76a7a247ac0db5a892677abba855d7a0a08
Nikita Shultais <nikita@shultais.ru>
2021-07-07 15:11:58 +0200

    Включил обработку media-файлов

--- dom_u_morya/urls.py
+++ dom_u_morya/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 from houses.views import houses_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', houses_list)
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
