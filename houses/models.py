f4fefd4bf5df1e43ebf701ae4029a0786d4d8397
Nikita Shultais <nikita@shultais.ru>
2021-07-07 15:11:18 +0200

    Добавил поле photo

--- houses/models.py
+++ houses/models.py
@@ -5,6 +5,7 @@ class House(models.Model):
     name = models.CharField("название", max_length=50)
     price = models.IntegerField("цена")
     description = models.TextField("описание")
+    photo = models.ImageField("фотография", upload_to="houses/photos", default="", blank=True)
 
     class Meta:
         verbose_name = "дом"
