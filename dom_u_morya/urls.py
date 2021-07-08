6381d8b8c99982bb4c2b4a8cadb896c4973a1da9
Nikita Shultais <nikita@shultais.ru>
2021-07-08 15:19:00 +0200

    Добавил страницу дома.

--- dom_u_morya/urls.py
+++ dom_u_morya/urls.py
@@ -17,11 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-from houses.views import houses_list
+from houses.views import houses_list, house_detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', houses_list)
+    path('', houses_list),
+    path("<int:house_id>", house_detail)
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- houses/views.py
+++ houses/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from houses.models import House
 
 
 def houses_list(request):
     houses = House.objects.all()
     return render(request, "houses/houses_list.html", {"houses": houses})
+
+
+def house_detail(request, house_id):
+    house = get_object_or_404(House, id=house_id)
+    return render(request, "houses/house_detail.html", {"house": house})
